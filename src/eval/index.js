4c5b2c8181b108fc7ce6092aea29dc8f04e8db75
Zach Sitler <zachsitler@gmail.com>
2017-05-10 21:23:18 -0700

    Formatting

--- src/eval/index.js
+++ src/eval/index.js
@@ -1,3 +1,3 @@
-export {default as Closure} from './closure'
-export {default as Environment} from './environment'
-export {default as Eval} from './eval'
+export { default as Closure } from './closure'
+export { default as Environment } from './environment'
+export { default as Eval } from './eval'
